0fd19508efb9f8e59c79a1f86d906b4744d5578e
Steve Fisher <dr.s.m.fisher@gmail.com>
2020-06-08 23:42:52 +0100

    Various changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# WordPress theme for croquet
+# WordPress theme for All Saints' Singers
 
 It provides a users.php to list members but nothing else at the moment
--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function croquet_enqueue_styles() {
+function ass_enqueue_styles() {
 
     $parent_style = 'twentyseventeen-style';
 
@@ -9,25 +9,6 @@ function croquet_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
-    wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.0.6/css/all.css'); 
 }
-add_action( 'wp_enqueue_scripts', 'croquet_enqueue_styles' );
-
-function mucd_primary_table_to_copy($primary_tables) {
-    $primary_tables[] = 'duplicator_packages';
-    $primary_tables[] = 'supsystic_tbl_columns';
-    $primary_tables[] = 'supsystic_tbl_diagrams';
-    $primary_tables[] = 'supsystic_tbl_rows';
-    $primary_tables[] = 'supsystic_tbl_tables';
-    $primary_tables[] = 'masterslider_sliders';
-    $primary_tables[] = 'masterslider_options';
-    return $primary_tables;
-}
-
-add_filter('mucd_default_primary_tables_to_copy', 'mucd_primary_table_to_copy');
-add_filter( 'allow_subdirectory_install', 
-  create_function( '', 'return true;' )
-);
-add_theme_support( 'sportspress' );
-
+add_action( 'wp_enqueue_scripts', 'ass_enqueue_styles' );
 ?>
